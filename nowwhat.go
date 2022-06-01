6ecd394da480bfc877d9eb6d203fbaa0f466e5d8
Palash Bauri <palashbauri1@gmail.com>
2022-06-01 21:01:15 +0530

    go fmt + fix

--- nowwhat.go
+++ nowwhat.go
@@ -6,14 +6,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-    weatherapi "pb/nowwhat/weather"
-    "reflect"
+	weatherapi "pb/nowwhat/weather"
+	"reflect"
 )
 
+func main() {
 
-func main(){
-    
-    text := `Welcome
+	text := `Welcome
     get IST (+0530) current time at =>
         /time/ist
     UTC time =>
@@ -21,49 +20,47 @@ func main(){
     
     `
 
-    r := gin.Default()
-    r.GET("/" , func(c *gin.Context){
-        
-        c.String(200 ,text)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
 
-    })
-    r.GET("/time/ist" , func(c *gin.Context){
-        
-        c.JSON(200 , gin.H{
-            "ist" : time.Now().Format("15:04:05.000000 IST"),
-        })
+		c.String(200, text)
 
+	})
+	r.GET("/time/ist", func(c *gin.Context) {
 
-    })
+		c.JSON(200, gin.H{
+			"ist": time.Now().Format("15:04:05.000000 IST"),
+		})
 
-    r.GET("/time/utc" , func(c *gin.Context){
-        
-        c.JSON(200 , gin.H{
-            "utc" : time.Now().UTC().Format("15:04:05.000000"),
-        })
+	})
 
+	r.GET("/time/utc", func(c *gin.Context) {
 
-    })
+		c.JSON(200, gin.H{
+			"utc": time.Now().UTC().Format("15:04:05.000000"),
+		})
 
-    r.GET("w/:location" , func(c *gin.Context){
-        location := c.Params.ByName("location")
-        wdata := weatherapi.GetWeather(location)
-        r:= reflect.ValueOf(wdata)
-        tempc := reflect.Indirect(r).FieldByName("tempc").Float()
-        status := reflect.Indirect(r).FieldByName("status").String()
-        lat :=  reflect.Indirect(r).FieldByName("lat").Float()
-        long := reflect.Indirect(r).FieldByName("long").Float()
-        
-        c.JSON(200 , gin.H{
-            "tempc" : tempc,
-            "weather" : status,
-            "lat" : lat,
-            "long" : long,
-        })
+	})
 
-    })
-    
-    fmt.Println("Starting server at http://localhost:8080/")
-    r.Run()
+	r.GET("w/:location", func(c *gin.Context) {
+		location := c.Params.ByName("location")
+		wdata := weatherapi.GetWeather(location)
+		r := reflect.ValueOf(wdata)
+		tempc := reflect.Indirect(r).FieldByName("tempc").Float()
+		status := reflect.Indirect(r).FieldByName("status").String()
+		lat := reflect.Indirect(r).FieldByName("lat").Float()
+		long := reflect.Indirect(r).FieldByName("long").Float()
+
+		c.JSON(200, gin.H{
+			"tempc":   tempc,
+			"weather": status,
+			"lat":     lat,
+			"long":    long,
+		})
+
+	})
+
+	fmt.Println("Starting server at http://localhost:8080/")
+	r.Run()
 
 }
--- weather/weather.go
+++ weather/weather.go
@@ -8,37 +8,32 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
-func noterr(e error){
-    if e != nil{
-        log.Fatalln(e)
-    }
+func noterr(e error) {
+	if e != nil {
+		log.Fatalln(e)
+	}
 }
 
-
-type Weather struct{
-    tempc float64
-    lat float64
-    long float64
-    status string
+type Weather struct {
+	tempc  float64
+	lat    float64
+	long   float64
+	status string
 }
-
 
 //return data => Temp in Celcius, Lat , Long , Weather
 func GetWeather(location string) Weather {
-    
-    godotenv.Load(".env")
-    var apikey = os.Getenv("OWM_API_KEY")
 
-    w, err := owm.NewCurrent("C" , "en" , apikey)
-    
-    noterr(err)
+	godotenv.Load(".env")
+	var apikey = os.Getenv("OWM_API_KEY")
 
-    w.CurrentByName(location)
-    
-    result := Weather{ tempc : w.Main.Temp  , lat : w.GeoPos.Latitude , long : w.GeoPos.Longitude , status : w.Weather[0].Description}
-    return result
-    
-    
+	w, err := owm.NewCurrent("C", "en", apikey)
+
+	noterr(err)
+
+	w.CurrentByName(location)
+
+	result := Weather{tempc: w.Main.Temp, lat: w.GeoPos.Latitude, long: w.GeoPos.Longitude, status: w.Weather[0].Description}
+	return result
 
 }
